2647264959f7870343026457ff065462693f0145
Artem <artemyo17@gmail.com>
2025-01-19 21:45:12 +0300

    Changed sprites and added animations, also added explosion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 import pygame
 
 pygame.init()
@@ -10,8 +11,6 @@ speed_move = 180
 speed_bullet = 400
 clock = pygame.time.Clock()
 bullet_status = 0
-enemy_direction = 1
-enemy_speed = 2
 
 player_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
@@ -20,6 +19,7 @@ enemy_group = pygame.sprite.Group()
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
@@ -27,77 +27,35 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Menu:
-    def __init__(self):
-        self.font = pygame.font.Font(None, 74)
-        self.font_small = pygame.font.Font(None, 36)
-        self.difficulty = "easy"
-        self.difficulties = ["easy", "medium", "hard"]
-        self.current_difficulty_index = 0
-        self.running = True
-
-    def draw(self):
-        screen.fill((0, 0, 0))
-        background = load_image("space_background.jpg")
-        screen.blit(background, (0, 0))
-        title = self.font.render("Space Invaders", True, (255, 255, 255))
-        start_button = self.font_small.render("Начать", True, (255, 255, 255))
-        exit_button = self.font_small.render("Выход", True, (255, 255, 255))
-        difficulty_text = self.font_small.render(f"Сложность: {self.difficulties[self.current_difficulty_index]}", True,
-                                                 (255, 255, 255))
-
-        start_button_rect = start_button.get_rect(center=(width // 2, height // 2))
-        exit_button_rect = exit_button.get_rect(center=(width // 2, height // 2 + 50))
-
-        screen.blit(title, (width // 2 - title.get_width() // 2, height // 4))
-        screen.blit(start_button, start_button_rect)
-        screen.blit(exit_button, exit_button_rect)
-        screen.blit(difficulty_text, (width // 2 - difficulty_text.get_width() // 2, height // 2 + 100))
-
-        left_arrow = self.font_small.render("<", True, (255, 255, 255))
-        right_arrow = self.font_small.render(">", True, (255, 255, 255))
-        screen.blit(left_arrow, (width // 2 - difficulty_text.get_width() // 2 - 30, height // 2 + 100))
-        screen.blit(right_arrow, (width // 2 + difficulty_text.get_width() // 2 + 10, height // 2 + 100))
-
-        return start_button_rect, exit_button_rect
-
-    def run(self):
-        while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    mouse_pos = pygame.mouse.get_pos()
-                    if start_button_rect.collidepoint(mouse_pos):
-                        return self.difficulties[self.current_difficulty_index]  # Начать игру с выбранной сложностью
-                    if exit_button_rect.collidepoint(mouse_pos):
-                        self.running = False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        self.current_difficulty_index = (self.current_difficulty_index + 1) % len(self.difficulties)
-                    elif event.key == pygame.K_DOWN:
-                        self.current_difficulty_index = (self.current_difficulty_index - 1) % len(self.difficulties)
-                    elif event.key == pygame.K_LEFT:
-                        self.current_difficulty_index = (self.current_difficulty_index - 1) % len(self.difficulties)
-                    elif event.key == pygame.K_RIGHT:
-                        self.current_difficulty_index = (self.current_difficulty_index + 1) % len(self.difficulties)
-
-            start_button_rect, exit_button_rect = self.draw()
-            pygame.display.flip()
-            clock.tick(fps)
-
-
 class Player(pygame.sprite.Sprite):
-    image = load_image("player.png")
+    image = load_image("ship.png")
 
     def __init__(self):
         super().__init__(player_group)
-        self.image = Player.image
+        self.frames = []
+        self.cut_sheet(Player.image, 5, 2)
+        self.if_num = 0
+        self.cur_frame = 0
+        self.image = pygame.transform.scale(self.frames[self.cur_frame], (32, 48))
         self.rect = self.image.get_rect()
         self.rect.x = width // 2 - self.rect.w // 2
         self.rect.y = 0.9 * height - self.rect.h // 2
 
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
     def update(self, *args):
+        if self.if_num == 5:
+            self.if_num = 0
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = pygame.transform.scale(self.frames[self.cur_frame], (32, 48))
+        self.if_num += 1
         global bullet_status
         if pygame.key.get_pressed()[pygame.K_SPACE]:
             if not bullet_status:
@@ -110,13 +68,15 @@ class Player(pygame.sprite.Sprite):
 
 
 class Bullet(pygame.sprite.Sprite):
+    image = load_image("laser-bolts.png")
+
     def __init__(self):
         super().__init__(bullet_group)
-        self.image = pygame.Surface((5, 15))
-        self.image.fill("WHITE")
+        self.frames = []
+        self.image = pygame.transform.scale(Bullet.image, (10, 24))
         self.rect = self.image.get_rect()
         self.rect.x = (list(player_group)[0].rect.x
-                       + list(player_group)[0].rect.w // 2 - self.rect.w // 2)
+                       + list(player_group)[0].rect.w // 2 - list(bullet_group)[0].rect.w // 2)
         self.rect.y = list(player_group)[0].rect.y - self.rect.h
 
     def update(self, *args):
@@ -128,116 +88,171 @@ class Bullet(pygame.sprite.Sprite):
 
 
 class Enemy_Red(pygame.sprite.Sprite):
-    image = load_image("red.png")
+    image = load_image("enemy-big.png")
 
     def __init__(self, x, y):
         super().__init__(enemy_group)
-        self.image = Enemy_Red.image
+        self.frames = []
+        self.cut_sheet(Enemy_Red.image, 2, 1)
+        self.cur_frame = 0
+        self.if_num = 0
+        self.image = pygame.transform.scale(self.frames[self.cur_frame], (52, 64))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
     def update(self, *args):
+        if self.if_num == 5:
+            self.if_num = 0
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = pygame.transform.scale(self.frames[self.cur_frame], (52, 64))
+        self.if_num += 1
         global bullet_status
         if pygame.sprite.spritecollideany(self, bullet_group):
+            Explosion(self.rect.x, self.rect.y, 52, 64)
             self.kill()
             list(bullet_group)[0].kill()
             bullet_status = 0
-
-        global enemy_direction
-        self.rect.x += enemy_direction * enemy_speed
-        if self.rect.right >= width or self.rect.left <= 0:
-            enemy_direction *= -1
-            for enemy in enemy_group:
-                enemy.rect.y += 10
 
 
 class Enemy_Yellow(pygame.sprite.Sprite):
-    image = load_image("yellow.png")
+    image = load_image("enemy-medium.png")
 
     def __init__(self, x, y):
         super().__init__(enemy_group)
-        self.image = Enemy_Yellow.image
+        self.frames = []
+        self.cut_sheet(Enemy_Yellow.image, 2, 1)
+        self.cur_frame = 0
+        self.if_num = 0
+        self.image = pygame.transform.scale(self.frames[self.cur_frame], (48, 24))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
     def update(self, *args):
+        if self.if_num == 5:
+            self.if_num = 0
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = pygame.transform.scale(self.frames[self.cur_frame], (48, 24))
+        self.if_num += 1
         global bullet_status
         if pygame.sprite.spritecollideany(self, bullet_group):
+            Explosion(self.rect.x, self.rect.y, 48, 24)
             self.kill()
             list(bullet_group)[0].kill()
             bullet_status = 0
-
-        global enemy_direction
-        self.rect.x += enemy_direction * enemy_speed
-        if self.rect.right >= width or self.rect.left <= 0:
-            enemy_direction *= -1
-            for enemy in enemy_group:
-                enemy.rect.y += 10
 
 
 class Enemy_Green(pygame.sprite.Sprite):
-    image = load_image("green.png")
+    image = load_image("enemy-small.png")
 
     def __init__(self, x, y):
         super().__init__(enemy_group)
-        self.image = Enemy_Green.image
+        self.frames = []
+        self.cut_sheet(Enemy_Green.image, 2, 1)
+        self.cur_frame = 0
+        self.if_num = 0
+        self.image = pygame.transform.scale(self.frames[self.cur_frame], (32, 32))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
     def update(self, *args):
+        if self.if_num == 5:
+            self.if_num = 0
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = pygame.transform.scale(self.frames[self.cur_frame], (32, 32))
+        self.if_num += 1
         global bullet_status
         if pygame.sprite.spritecollideany(self, bullet_group):
+            Explosion(self.rect.x, self.rect.y, 32, 32)
             self.kill()
             list(bullet_group)[0].kill()
             bullet_status = 0
 
-        global enemy_direction
-        self.rect.x += enemy_direction * enemy_speed
-        if self.rect.right >= width or self.rect.left <= 0:
-            enemy_direction *= -1
-            for enemy in enemy_group:
-                enemy.rect.y += 10
+class Explosion(pygame.sprite.Sprite):
+    image = load_image("explosion.png")
+
+    def __init__(self, x, y, x_size, y_size):
+        super().__init__(enemy_group)
+        self.frames = []
+        self.cut_sheet(Explosion.image, 5, 1)
+        self.cur_frame = 0
+        self.if_num = 0
+        self.y_size = y_size
+        self.image = pygame.transform.scale(self.frames[self.cur_frame], (self.y_size, self.y_size))
+        self.rect = self.image.get_rect()
+        self.rect.x = x - y_size // 2 + x_size // 2
+        self.rect.y = y
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self, *args):
+        global bullet_status
+        if self.if_num == 3:
+            self.if_num = 0
+            self.cur_frame += 1
+            if self.cur_frame == 5:
+                self.kill()
+            else:
+                self.image = pygame.transform.scale(self.frames[self.cur_frame], (self.y_size, self.y_size))
+        self.if_num += 1
 
 
-def main():
-    menu = Menu()
-    difficulty = menu.run()
+Player()
+for i in range(22, width - 50, 56):
+    Enemy_Red(i, 40)
+for i in range(40, width - 50, 52):
+    Enemy_Yellow(i, 115)
+for i in range(57, width - 50, 50):
+    Enemy_Green(i, 150)
 
-    if difficulty is None:
-        pygame.quit()
-        return
-
-    player = Player()
-
-    enemy_count = 10
-
-    for i in range(enemy_count):
-        x_position = 50 + (i * (width - 100) // enemy_count)
-        Enemy_Red(x_position, 50)
-        Enemy_Yellow(x_position, 100)
-        Enemy_Green(x_position, 150)
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        screen.fill((0, 0, 0))
-        player_group.draw(screen)
-        player_group.update()
-        bullet_group.draw(screen)
-        bullet_group.update()
-        enemy_group.draw(screen)
-        enemy_group.update()
-        pygame.display.flip()
-        clock.tick(fps)
-
-    pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    player_group.draw(screen)
+    player_group.update(event)
+    bullet_group.draw(screen)
+    bullet_group.update(event)
+    enemy_group.draw(screen)
+    enemy_group.update(event)
+    pygame.display.flip()
+    clock.tick(fps)
+pygame.quit()
